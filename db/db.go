73228a4a8adbf1707c84cc775c355b1f110b5ee3
hard-working-He <2380588373@qq.com>
2025-04-30 16:19:18 +0800

    fix

--- db/db.go
+++ db/db.go
@@ -16,7 +16,7 @@ var (
 // GetDB returns a database connection
 func GetDB() *gorm.DB {
 	once.Do(func() {
-		dsn := "root:Henian2345..@tcp(127.0.0.1:3306)/videos_db?charset=utf8mb4&parseTime=True&loc=Local"
+		dsn := "root:Henian2345..@tcp(81.68.224.194:3306)/videos_db?charset=utf8mb4&parseTime=True&loc=Local"
 		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 		if err != nil {
 			log.Fatal("Failed to connect to database:", err)
